6516b168ae142a8a2f47ae561dfe97a7a3300a13
adamdgit <adammdemol@outlook.com>
2025-07-19 22:43:24 +1000

    Improved logic, added optional close button

--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -1,9 +1,19 @@
-import { useRef } from "react";
-import type { DialogProps, DialogWrapperProps } from "../../types";
-import styles from "./dialog.module.css"
+import { useContext, useEffect, useRef, useState } from "react";
+import styles from "./dialog.module.css";
+import type { DialogButtonProps, DialogProps, DialogWrapperProps } from "../../types";
+import { DialogContext, DialogWrapperContext } from "../../context";
 
 function Dialog({ className, children, style, showDialog, onClose }: DialogProps) {
+    const [show, setShow] = useState(showDialog);
     const ref = useRef<HTMLDivElement>(null);
+
+    // Listen for show Dialog updates from parent components
+    useEffect(() => {
+        setShow(showDialog);
+        if (!showDialog) {
+            onClose();
+        }
+    },[showDialog])
 
     function handleOnBlur(e: React.MouseEvent<HTMLDivElement>) {
         // Because dialog component uses inset 0 it fills the entire page
@@ -13,33 +23,62 @@ function Dialog({ className, children, style, showDialog, onClose }: DialogProps
         }
     }
     
-    if (showDialog) {
+    if (show) {
         return (
-            <div
-                ref={ref}
-                style={style}
-                className={className ?? styles.dialog} 
-                onClick={handleOnBlur}
-            >
-                <DialogWrapper>
+            <DialogContext.Provider value={{ onClose, setShow }}>
+                <div
+                    ref={ref}
+                    style={style}
+                    className={className ?? styles.dialog} 
+                    onClick={handleOnBlur}
+                >
                     {children}
-                </DialogWrapper>
-            </div>
+                </div>
+            </DialogContext.Provider>
         )
     }
-}
+};
 
 function DialogWrapper({ children }: DialogWrapperProps) {
+    const context = useContext(DialogContext);
+    if (!context) throw new Error("DialogWrapper must be a child of Dialog component")
+
+    const { onClose, setShow } = context;
+
     return (
-        <div 
-            className={styles.dialogWrapper}
-        >
-            {children}
-        </div>
+        <DialogWrapperContext.Provider value={{ onClose, setShow }}>
+            <div 
+                className={styles.dialogWrapper}
+            >
+                {children}
+            </div>
+        </DialogWrapperContext.Provider>
     )
-}
+};
+
+function DialogCloseButton({ className }: DialogButtonProps) {
+    const context = useContext(DialogWrapperContext);
+    if (!context) throw new Error("DialogCloseButton must be a child of DialogWrapper component");
+
+    const { onClose, setShow } = context;
+
+    function handleCloseDialog() {
+        setShow(false);
+        onClose();
+    }
+
+    return (
+        <button 
+            onClick={handleCloseDialog}
+            className={className ?? styles.dialogBtn}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width={12} viewBox="0 0 384 512">{'<!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->'}<path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+        </button>
+    )
+};
 
 export {
     Dialog,
-    DialogWrapper
+    DialogWrapper,
+    DialogCloseButton
 }
