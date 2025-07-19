b6e4ab9f9aecea79e28965da64b28d40dc7b5092
adamdgit <adammdemol@outlook.com>
2025-07-20 00:42:46 +1000

    reduced code complexity for progress bar

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef } from "react";
 import styles from "./toast.module.css"
 import type { ToastContentProps, ToastProps } from "../../types";
 import { ToastContext } from "../../context";
@@ -24,7 +24,7 @@ function Toast({ className, style, children, position, timeoutDuration, showToas
         return (
             <ToastContext.Provider value={{ showToast, timeoutDuration, progressBar, onClose }}>
                 <div 
-                    className={`${className ?? styles.toast} ${showToast ? styles.show : ''}`} 
+                    className={className ?? styles.toast} 
                     style={styleOverride}
                     role="status"
                     aria-atomic="true"
@@ -65,43 +65,26 @@ function ProgressBar() {
     const context = useContext(ToastContext);
     if (!context) throw new Error("Missing context provider");
 
-    const { showToast, timeoutDuration, progressBar, onClose } = context;
-    const [percent, setPercent] = useState(100);
-    const INTERVAL = 50;
+    const ref = useRef<HTMLDivElement>(null);
+    const { timeoutDuration, progressBar, onClose } = context;
 
     useEffect(() => {
-        // if parent ever updates the show to false, we reset the percentage
-        if (!showToast) {
-            setTimeout(() => {
-                setPercent(100);
-            }, 150);
-            return
-        }
+        if (!ref.current) return
+        // reset width back to full, whenever component mounts
+        ref.current.style.width = '100%';
+        ref.current.style.setProperty('--duration', `${timeoutDuration}ms`);
 
-        // work out the interval to update the progress bar, based on 100% - 0% and duration
-        const updateInterval = 100 / (timeoutDuration / INTERVAL);
-
-        // Reduce percentage based on timeoutDuration
-        const timer = setInterval(() => {
-            setPercent(prev => prev -= updateInterval);
-        }, INTERVAL);
+        // close toast after timeoutDuration (milliseconds)
+        const timer = setTimeout(() => {
+            onClose();
+        }, timeoutDuration);
 
         return () => clearInterval(timer)
-    },[showToast])
-
-    useEffect(() => {
-        if (percent <= 0) {
-            onClose();
-        }
-    }, [percent]);
+    },[ref, timeoutDuration])
 
     if (progressBar) {
         return (
-            <div 
-                className={styles.toastProgress} 
-                style={{width: `${percent}%`}}
-            >
-            </div>
+            <div className={styles.toastProgress} ref={ref}></div>
         ) 
     }
 }
