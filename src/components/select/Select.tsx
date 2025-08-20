ec3b70a162f15640e9b3761457f721946ac4549f
adamdgit <adammdemol@outlook.com>
2025-08-20 15:02:40 +1000

    Modified Select dropdown component structure

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,16 +1,18 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import styles from "./select.module.css"
-import type { SelectItemProps, SelectProps } from "../../types";
+import type { SelectItemProps, SelectListProps, SelectProps } from "../../types";
 import { SelectContext } from "../../context";
 
-function Select({ className, children, id, label, defaultValue, onChange }: SelectProps) {
+//--------------------------------------------------------------------//
+
+function Select({ value, label, classMap, children, id, onChange, ...props }: SelectProps) {
     const inputRef = useRef<HTMLInputElement | null>(null);
     const [showList, setShowList] = useState(false);
-    const [selected, setSelected] = useState('');
-    const [listItems, setListItems] = useState<(HTMLLIElement | HTMLDivElement)[]>([]);
-
     const [currentIndex, setCurrentIndex] = useState(0);
     const ref = useRef<HTMLDivElement | null>(null);
+    const [listItems, setListItems] = useState<(HTMLLIElement | HTMLDivElement)[]>([]);
+    const [internalVal, setInternalVal] = useState<string | number>(value ?? "");
+    const isControlled = value !== undefined;
 
     // save reference to each list item in the select dropdown
     useEffect(() => {
@@ -31,7 +33,7 @@ function Select({ className, children, id, label, defaultValue, onChange }: Sele
 
             setShowList(false);
         }
-    }
+    };
 
     const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
         if (e.key === "Enter") setShowList(!showList); // toggle show
@@ -61,15 +63,16 @@ function Select({ className, children, id, label, defaultValue, onChange }: Sele
                     break;
             }
         }
-    }
+    };
 
     return (
         <SelectContext.Provider value={{ 
-            listItems, setListItems, inputRef, onChange, setSelected, showList, setCurrentIndex
+            id, listItems, setListItems, inputRef, onChange, showList, setCurrentIndex, isControlled, setInternalVal
         }}>
             <div 
+                {...props}
                 ref={ref}
-                className={styles.selectWrap}
+                className={classMap?.selectInput ?? styles.selectWrap}
                 onClick={() => setShowList(!showList)}
                 tabIndex={0}
                 onKeyDown={handleKeyPress}
@@ -84,7 +87,7 @@ function Select({ className, children, id, label, defaultValue, onChange }: Sele
                     htmlFor={id} 
                     className={styles.selectLabel}
                 >
-                    {selected === '' ? label : selected}
+                    {value ? value : internalVal ? internalVal : label}
                 </label>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width={12} viewBox="0 0 512 512">{`<!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->`}<path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
                 <input
@@ -92,16 +95,9 @@ function Select({ className, children, id, label, defaultValue, onChange }: Sele
                     aria-hidden="true"
                     type="hidden"
                     id={id}
-                    value={defaultValue}
+                    value={value ?? internalVal}
                 />
-
-                <ul 
-                    id={`${id}-dropdown`}
-                    className={`${className ?? styles.selectList} ${showList ? styles.show : ""}`} 
-                    role="listbox"
-                >
-                    {children}
-                </ul>
+                {children}
             </div>
         </SelectContext.Provider>
     );
@@ -109,12 +105,33 @@ function Select({ className, children, id, label, defaultValue, onChange }: Sele
 
 //--------------------------------------------------------------------//
 
-function SelectItem({ className, children, value }: SelectItemProps) {
+function SelectList({ children, className, style, ...props}: SelectListProps) {
     const context = useContext(SelectContext);
-    if (!context) throw new Error("Missing context provider")
+    if (!context) throw new Error("SelectList must be a child of Select");
+
+    const { id, showList } = context;
+
+    return (
+        <ul 
+            {...props}
+            id={`${id}-dropdown`}
+            style={style}
+            className={`${className ?? styles.selectList} ${showList ? styles.show : ""}`} 
+            role="listbox"
+        >
+            {children}
+        </ul>
+    )
+}
+
+//--------------------------------------------------------------------//
+
+function SelectItem({ className, children, value, ...props }: SelectItemProps) {
+    const context = useContext(SelectContext);
+    if (!context) throw new Error("SelectItem must be a child of SelectList")
 
     const { 
-        setSelected, setCurrentIndex, listItems, setListItems, inputRef, onChange 
+        listItems, setListItems, setCurrentIndex, inputRef, onChange, isControlled, setInternalVal
     } = context;
     const ref = useRef(null);
 
@@ -127,17 +144,17 @@ function SelectItem({ className, children, value }: SelectItemProps) {
         }
     }, [ref]);
 
-    const  handleSelectItem = (e:  React.MouseEvent<HTMLLIElement> | React.KeyboardEvent<HTMLLIElement>) => {
+    const handleSelectItem = (e:  React.MouseEvent<HTMLLIElement> | React.KeyboardEvent<HTMLLIElement>) => {
         if (!inputRef.current) return
         
         // set value for the hidden input
         const targetValue = e.currentTarget.dataset.value ?? '';
-        const targetText = e.currentTarget.innerText;
-
         inputRef.current.value = targetValue;
 
         onChange(targetValue);
-        setSelected(targetText);
+        if (!isControlled) {
+            setInternalVal(targetValue);
+        }
         // Reset selected index to 0, focus dropdown
         setCurrentIndex(0);
         listItems[0].focus();
@@ -149,6 +166,7 @@ function SelectItem({ className, children, value }: SelectItemProps) {
 
     return (
         <li 
+            {...props}
             ref={ref}
             className={className ?? styles.selectItem}
             data-value={value}
@@ -164,5 +182,6 @@ function SelectItem({ className, children, value }: SelectItemProps) {
 
 export {
     Select,
+    SelectList,
     SelectItem
 };
